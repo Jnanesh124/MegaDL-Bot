e52525e9ae5a4888af44d0029d1e39ac7f4fae41
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-01-18 16:31:10 +0600

    Update caption.py

--- megadl/caption.py
+++ megadl/caption.py
@@ -1,12 +1,10 @@
-# (c) @doreamonfans1 
-# A Part of MegaDL-Bot <https://github.com/disneyteam77/MegaDL-Bot>
-
 import os
 from pyrogram import Client, filters
 from config import Config
 
-@Client.on_message(filters.reply & filters.text & filters.private & ~filters.edited)
+@Client.on_message(filters.reply & filters.text & filters.private)
 async def newcap(bot, message):
     nc = message.reply_to_message
+    # Check if the replied message contains media and is not a video note or sticker
     if nc.media and not (nc.video_note or nc.sticker):
         await nc.copy(message.chat.id, caption=message.text)
